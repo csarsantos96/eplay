c3473fa110dd62dbd9385163b04a7a1dcefb0bc1
Cesar Santos <csar.santos18@gmail.com>
2025-01-07 16:42:55 -0500

    loja de video games - Products

---
+++ src/components/Product/insdex.tsx
@@ -0,0 +1,11 @@
+const Product = () => {
+  ;<div>
+    <img src="" alt="" />
+    <h3>Nome do jogo</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, voluptate
+      illo porro a illum temporibus totam ex soluta velit reiciendis amet
+      repudiandae saepe esse similique atque, numquam quis vel dolorem?
+    </p>
+  </div>
+}
