e6b52dad8a8f8bb9e5f9e145bd5169a35bb8815a
Cesar Santos <csar.santos18@gmail.com>
2025-01-10 16:36:24 -0500

    feat: adicionando o Hero e algumas alterações no estilo global do Button

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,3 +1,32 @@
-const Hero = () => <div>hero</div>
+import bannerImg from '../../assets/images/fundo_hogwarts.png'
+import Button from '../Button'
+import Tag from '../Tag'
+
+import { Banner, Infos } from './styles'
+
+const Hero = () => (
+  <Banner style={{ backgroundImage: `url(${bannerImg})` }}>
+    <div className="container">
+      <div>
+        <Tag size={'small'}>RPG</Tag>
+        <Tag size={'small'}>PS5</Tag>
+      </div>
+      <Infos>
+        <h2>Hogwarts Legacy</h2>
+        <p>
+          <span>De R$ 250,00</span>
+          Por R$ 190,00
+        </p>
+        <Button
+          type="button"
+          title="Clique aqui para adicionar esse jogo ao carrinho"
+          variant="primary"
+        >
+          Adicionar ao carrinho
+        </Button>
+      </Infos>
+    </div>
+  </Banner>
+)
 
 export default Hero
