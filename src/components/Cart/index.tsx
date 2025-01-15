b4912fa981dd3be661c80cfadc51ec6be4c87eb4
Cesar Santos <csar.santos18@gmail.com>
2025-01-15 11:57:02 -0500

    feat: criando componente para o carrinho, listas de produtos e configurar abertura e fechamento dos botões

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -11,42 +11,55 @@ import {
   CartItem
 } from './styles'
 import Tag from '../Tag'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+import { close } from '../../store/reducers/cart'
 
-const Cart = () => (
-  <CartContainer>
-    <OverLay />
-    <Sidebar>
-      <ul>
-        <CartItem>
-          <img src={starWars} alt="star wars banner" />
-          <div>
-            <h3>Nome do Jogo</h3>
-            <Tag size={'small'}>RPG</Tag>
-            <Tag size={'small'}>ps5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-        <CartItem>
-          <img src={starWars} alt="star wars banner" />
-          <div>
-            <h3>Nome do Jogo</h3>
-            <Tag size={'small'}>RPG</Tag>
-            <Tag size={'small'}>ps5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-      </ul>
-      <Quantity> 2 jogo(s) no carrinho</Quantity>
-      <Prices>
-        Total de R$ 250,00 <span>Em até 6x sem juros</span>
-      </Prices>
-      <Button title="Clique aqui para continuar com a compra" type="button">
-        Continuar com a compra
-      </Button>
-    </Sidebar>
-  </CartContainer>
-)
+const Cart = () => {
+  const { isOpen } = useSelector((state: RootReducer) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <OverLay onClick={closeCart} />
+      <Sidebar>
+        <ul>
+          <CartItem>
+            <img src={starWars} alt="star wars banner" />
+            <div>
+              <h3>Nome do Jogo</h3>
+              <Tag size={'small'}>RPG</Tag>
+              <Tag size={'small'}>ps5</Tag>
+              <span>R$ 150,00</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+          <CartItem>
+            <img src={starWars} alt="star wars banner" />
+            <div>
+              <h3>Nome do Jogo</h3>
+              <Tag size={'small'}>RPG</Tag>
+              <Tag size={'small'}>ps5</Tag>
+              <span>R$ 150,00</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+        </ul>
+        <Quantity> 2 jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total de R$ 250,00 <span>Em até 6x sem juros</span>
+        </Prices>
+        <Button title="Clique aqui para continuar com a compra" type="button">
+          Continuar com a compra
+        </Button>
+      </Sidebar>
+    </CartContainer>
+  )
+}
 
 export default Cart
